701ad30ba514e32f9b31cbf8d81fc0060c0728a7
Marcelo Prates <mpratesjunior@gmail.com>
2026-03-06 16:25:33 -0300

    first commit

---
+++ src/main/java/io/github/pratesjr/nutriledgerapi/NutriLedgerApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.pratesjr.nutriledgerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriLedgerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutriLedgerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/pratesjr/nutriledgerapi/NutriLedgerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pratesjr.nutriledgerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriLedgerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
