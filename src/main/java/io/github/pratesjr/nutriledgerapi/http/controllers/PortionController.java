302592a441b345768ca74a823693604f076f8d30
Marcelo Prates <mpratesjunior@gmail.com>
2026-03-25 16:15:48 -0300

    fix(#2): review from copilot requested changes

--- src/main/java/io/github/pratesjr/nutriledgerapi/http/controllers/PortionController.java
+++ src/main/java/io/github/pratesjr/nutriledgerapi/http/controllers/PortionController.java
@@ -14,6 +14,11 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @Tag(name = "Portions", description = "Portion management endpoints")
 @RestController
